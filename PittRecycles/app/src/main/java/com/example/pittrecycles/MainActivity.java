22d900f666ed9b46880510da70a759b0f9b98fd5
Peter Mulholland <pem42@pitt.edu>
2019-03-01 23:17:28 -0500

    Add android studio project

---
+++ PittRecycles/app/src/main/java/com/example/pittrecycles/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pittrecycles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button myButton = (Button) findViewById(R.id.my_button);
+
+    }
+}
