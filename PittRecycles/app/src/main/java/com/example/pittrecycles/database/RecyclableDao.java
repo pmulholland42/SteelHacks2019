ed0246a92555049c126a903611fd693e1c099ea8
Tyler Bobik <shed412@gmail.com>
2019-03-02 01:47:22 -0500

    made dao

---
+++ PittRecycles/app/src/main/java/com/example/pittrecycles/database/RecyclableDao.java
@@ -0,0 +1,17 @@
+package com.example.pittrecycles.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+
+import com.example.pittrecycles.entities.Recyclable;
+
+@Dao
+public interface RecyclableDao {
+
+    @Query("select * from recs_table where barcode=(:barcode)")
+    Recyclable getForBaecode(int barcode);
+
+    @Query("select rec_type from recs_table where barcode=(:barcode)")
+    Recyclable.RecyclableType getTypeForBarcode(String barcode);
+
+}
--- PittRecycles/app/src/main/java/com/example/pittrecycles/entities/Recyclable.java
+++ PittRecycles/app/src/main/java/com/example/pittrecycles/entities/Recyclable.java
@@ -4,7 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "recs_table")
 public class Recyclable {
 
     @PrimaryKey
