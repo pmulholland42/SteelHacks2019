d960bf93c7932925f2ef0d9b98b3e13462d1666f
Tyler Bobik <shed412@gmail.com>
2019-03-02 01:30:01 -0500

    made getters and setters

--- PittRecycles/app/src/main/java/com/example/pittrecycles/entities/Recyclable.java
+++ PittRecycles/app/src/main/java/com/example/pittrecycles/entities/Recyclable.java
@@ -11,13 +11,49 @@ public class Recyclable {
     private int id;
 
     @ColumnInfo(name = "barcode")
-    private String barCode;
+    private int barCode;
 
     @ColumnInfo(name = "rec_type")
     private RecyclableType recyclableType;
 
     @ColumnInfo(name = "source")
     private Source source;
+
+    public Recyclable() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public RecyclableType getRecyclableType() {
+        return recyclableType;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public int getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(int barCode) {
+        this.barCode = barCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRecyclableType(RecyclableType recyclableType) {
+        this.recyclableType = recyclableType;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
 
     public enum RecyclableType {
         PLASTIC_1, PLASTIC_2, PLASTIC_3, PLASTIC_4, PLASTIC_5, PLASTIC_6, PLASTIC_7
